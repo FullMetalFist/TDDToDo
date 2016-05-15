f4554759c08bb1ea8d3ecb2185c5d2ce30638171
Michael Vilabrera <m.vilabrera@gmail.com>
2016-05-15 09:37:47 -0400

    implemented Location struct

---
+++ TDDToDoTests/Model/LocationTests.swift
@@ -0,0 +1,37 @@
+//
+//  LocationTests.swift
+//  TDDToDo
+//
+//  Created by Michael Vilabrera on 5/15/16.
+//  Copyright © 2016 Michael Vilabrera. All rights reserved.
+//
+
+import XCTest
+@testable import TDDToDo
+import CoreLocation
+
+class LocationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testInit_ShouldSetNameAndCoordinate() {
+        let testCoordinate = CLLocationCoordinate2D(latitude: 1, longitude: 2)
+        let location = Location(name: "", coordinate: testCoordinate)
+        
+        XCTAssertEqual(location.coordinate?.latitude, testCoordinate.latitude, "Initializer should set latitude")
+        XCTAssertEqual(location.coordinate?.longitude, testCoordinate.longitude, "Initializer should set longitude")
+    }
+    
+    func testInit_ShouldSetName() {
+        let location = Location(name: "Test name")
+        XCTAssertEqual(location.name, "Test name", "Initializer should set the name")
+    }
+}
