8413a5b547af8d81d2823da3f8cb721e996fb05d
Michael Vilabrera <m.vilabrera@gmail.com>
2016-05-15 08:51:02 -0400

    initial commit

---
+++ TDDToDoTests/Model/ToDoItemTests.swift
@@ -0,0 +1,36 @@
+//
+//  ToDoItemTests.swift
+//  TDDToDo
+//
+//  Created by Michael Vilabrera on 5/15/16.
+//  Copyright © 2016 Michael Vilabrera. All rights reserved.
+//
+
+import XCTest
+@testable import TDDToDo
+
+class ToDoItemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
