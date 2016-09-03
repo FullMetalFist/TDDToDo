050700ad1e40a7705e0e14a7780c08beba707fa3
Michael Vilabrera <m.vilabrera@gmail.com>
2016-09-02 22:31:45 -0400

    revert back to last working test

---
+++ TDDToDo/ViewControllers/InputViewController.swift
@@ -0,0 +1,34 @@
+//
+//  InputViewController.swift
+//  TDDToDo
+//
+//  Created by Michael Vilabrera on 8/28/16.
+//  Copyright © 2016 Michael Vilabrera. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ TDDToDoTests/ViewControllers/InputViewControllerTests.swift
@@ -0,0 +1,65 @@
+//
+//  InputViewControllerTests.swift
+//  TDDToDo
+//
+//  Created by Michael Vilabrera on 8/28/16.
+//  Copyright © 2016 Michael Vilabrera. All rights reserved.
+//
+
+import XCTest
+@testable import TDDToDo
+import CoreLocation
+
+public typealias CLGeocodeCompletionHander = ([CLPlacemark]?, NSError?) -> Void
+
+class InputViewControllerTests: XCTestCase {
+    
+    var sut: InputViewController!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewControllerWithIdentifier("InputViewController") as! InputViewController
+        
+        _ = sut.view
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_HasTitleTextField() {
+        XCTAssertNotNil(sut.titleTextField)
+    }
+    
+    /**
+     * Now it is time to create the tests independent of the author's direct walkthrough.
+     “dateTextField, locationTextField, addressTextField, descriptionTextField, saveButton, and cancelButton”
+     
+     Excerpt From: “Test-Driven iOS Development with Swift.” iBooks.
+     */
+    
+    func test_HasfiveTextFieldsAndTwoSaveButtons() {
+        XCTAssertNotNil(sut.dateTextField)
+        XCTAssertNotNil(sut.locationTextField)
+        XCTAssertNotNil(sut.addressTextField)
+        XCTAssertNotNil(sut.descriptionTextField)
+        XCTAssertNotNil(sut.saveButton)
+        XCTAssertNotNil(sut.cancelButton)
+    }
+    
+}
+
+extension InputViewControllerTests {
+    class MockGeocoder: CLGeocoder {
+        
+        var completionHandler: CLGeocodeCompletionHandler?
+        
+        override func geocodeAddressString(addressString: String, completionHandler: CLGeocodeCompletionHandler) {
+            
+            self.completionHandler = completionHandler
+        }
+    }
+}
